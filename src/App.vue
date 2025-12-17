7ed4550442be1a3772d4a7cc7c796f37539e16a8
JavierXool <javier.xool@outlook.com>
2025-12-17 14:24:01 -0600

    rounting in action

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
+    <div class="text-center pt-3 text-success h3">CountOpedia</div>
+    <hr />
+    <div class="row">
+      <div class="col-4 offset-1">
+        <router-link
+          to="/"
+          class="form-control badge rounded-pill text-bg-warning p-4"
+        >
+          Home
+        </router-link>
+      </div>
 
-    <div class="wrapper">
-      Hello World
+      <div class="col-4 offset-1">
+        <router-link
+          to="/info"
+          class="form-control badge rounded-pill text-bg-warning p-4"
+        >
+          Info
+        </router-link>
+      </div>
     </div>
   </header>
 
