af2d45d036561ed42f541308da38ddbc4a4bba23
JavierXool <javier.xool@outlook.com>
2025-12-17 13:21:26 -0600

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Home from "@/components/Home.vue";
+import Info from "@/components/Info.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/info",
+      name: "info",
+      component: Info,
+    },
+  ],
+});
+
+export default router;
