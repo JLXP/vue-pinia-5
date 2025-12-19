2187b52bd9c2537da53b3e0da5fc397fae6d1466
JavierXool <javier.xool@outlook.com>
2025-12-18 20:50:17 -0600

    getters in pinia and a pro

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,20 +6,38 @@
     <br />
     <div class="row">
       <div class="col-5 offset-1">
-        <button class="form-control btn btn-success p-4">Increment</button>
+        <button class="form-control btn btn-success p-4" @click="Increment">
+          Increment
+        </button>
       </div>
       <div class="col-5">
-        <button class="form-control btn btn-danger p-4">Decrement</button>
+        <button class="form-control btn btn-danger p-4" @click="Decrement">
+          Decrement
+        </button>
       </div>
       <div class="col-6 offset-3 pt-2">
-        <button class="form-control btn btn-warning p-4">Random</button>
+        <button class="form-control btn btn-warning p-4" @click="Random">
+          Random
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useGameStore } from '@/store/gameStore';
+import { useGameStore } from "@/store/gameStore";
+
+function Increment() {
+  gameStore.score += gameStore.getNextAttack;
+}
+
+function Decrement() {
+  gameStore.score -= gameStore.getNextDefense;
+}
+
+function Random() {
+  Math.random() > 0.5 ? Increment() : Decrement();
+}
 
 const gameStore = useGameStore();
 </script>
